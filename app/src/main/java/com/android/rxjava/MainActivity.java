b3f5e6c296615ab3b5308510d496938adfc361e3
sp <saiprasadreddy081@gmail.com>
2020-11-24 11:44:02 -0500

    disposables

--- app/src/main/java/com/android/rxjava/MainActivity.java
+++ app/src/main/java/com/android/rxjava/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView text;
     private CompositeDisposable disposables = new CompositeDisposable();
 
+    //disposables
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
